4cb486204b319b5cb358eff83d5102e9b9ae2738
matt <matt@mattmbp.home>
2022-06-23 17:42:11 +0100

    doc

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
-// import attachFastClick from "fastclick";
-// import App from "./App.svelte";
+import attachFastClick from "fastclick";
+import App from "./App.svelte";
 
-import {
-  createEthereumWallet,
-  createTezosWallet,
-  validateMnemonic,
-} from "./wallet";
+// import {
+//   createEthereumWallet,
+//   createTezosWallet,
+//   validateMnemonic,
+// } from "./wallet";
 
-(async () => {
-  const expected = {
-    mnemonic:
-      "stick attend canyon tray wisdom tobacco reveal trigger earn exhaust hazard fluid",
-    passphrase: "",
-    privateKey:
-      "edskSB1X9zwHssYiGFREFhK1zTgSSfjh58WmjvZPr3PWpKAdSquZxtPw996ZWuS645akADWp9EX6W3nV9rNSeWvtpgvWvNQ2Pt",
-    publicKey: "edpktfa9vrACQYZDoHw8viLrpQx5v93TMTXRa3DdZn3euwtHrXqH6e",
-    publicKeyHash: "tz1ggivFLf9EsPBaNaN8WL2yHQaGxBw74J2A",
-  };
-  const phrase =
-    "stick attend canyon tray wisdom tobacco reveal trigger earn exhaust hazard fluid";
-  const obj = await createTezosWallet(phrase);
-  console.log(obj);
-  const obj2 = await createEthereumWallet(phrase);
-  console.log(obj2);
-  console.log(validateMnemonic(phrase));
-})();
+// (async () => {
+//   const expected = {
+//     mnemonic:
+//       "stick attend canyon tray wisdom tobacco reveal trigger earn exhaust hazard fluid",
+//     passphrase: "",
+//     privateKey:
+//       "edskSB1X9zwHssYiGFREFhK1zTgSSfjh58WmjvZPr3PWpKAdSquZxtPw996ZWuS645akADWp9EX6W3nV9rNSeWvtpgvWvNQ2Pt",
+//     publicKey: "edpktfa9vrACQYZDoHw8viLrpQx5v93TMTXRa3DdZn3euwtHrXqH6e",
+//     publicKeyHash: "tz1ggivFLf9EsPBaNaN8WL2yHQaGxBw74J2A",
+//   };
+//   const phrase =
+//     "stick attend canyon tray wisdom tobacco reveal trigger earn exhaust hazard fluid";
+//   const obj = await createTezosWallet(phrase);
+//   console.log(obj);
+//   const obj2 = await createEthereumWallet(phrase);
+//   console.log(obj2);
+//   console.log(validateMnemonic(phrase));
+// })();
 
-// attachFastClick(document.body);
+attachFastClick(document.body);
 
-// const app = new App({
-//   target: document.body,
-// });
+const app = new App({
+  target: document.body,
+});
 
-// export default app;
+export default app;
